463a85613dde7b4ff95494e6baad205acbcd1328
Revarsel <mastermindofall111@gmail.com>
2024-09-29 20:00:28 +0530

    changed some stuff

--- otherpythonstuff/csv_act_id_to_std_map.py
+++ otherpythonstuff/csv_act_id_to_std_map.py
@@ -1,21 +1,8 @@
 import csv
 
-skill_data = []
-with open("associated_skill.csv", "r") as file2:
-    data = csv.reader(file2)
-    
-    for i in data:
-        skill_data.append([i[0], i[1]])
+file_data = []
 
-skill_data = skill_data[1:]
-# print(skill_data)
-
-dictionary = {}
-
-for i in skill_data:
-    dictionary[i[1]] = i[0]
-
-with open("Only_Activity_Database_Entry_file.csv", "r") as file:
+with open("otherpythonstuff/Only_Activity_Database_Entry_file1.csv", "r") as file: # act to associated
     data = csv.reader(file, delimiter=',')
     count = 0
     for i in data:
@@ -24,16 +11,14 @@ with open("Only_Activity_Database_Entry_file.csv", "r") as file:
             continue
         local = []
         local.append(i[0])
-        i.pop(1)
         i = i[1:5]
         for k in i:
             if k != "None":
-                local.append(dictionary[k])
-        print(local)
+                local.append(k)
+        file_data.append(local)
 
-# file_data = []
 
-# with open("Only_Activity_Database_Entry_file.csv", "r") as file:
+# with open("otherpythonstuff/Only_Activity_Database_Entry_file1.csv", "r") as file: # act to standard
 #     data = csv.DictReader(file, delimiter=',')
 #     for i in data:
 #         local = []
@@ -45,17 +30,115 @@ with open("Only_Activity_Database_Entry_file.csv", "r") as file:
 #         keys.pop(1)
 #         keys.pop(1)
 #         keys.pop(0)
+#         keys = keys[0:12]
 #         for k in keys:
 #             if i[k] != "None":
 #                 local.append(k)
 #         file_data.append(local)
 
-# string = ""
-# for i in file_data:
-#     act_id = i[0]
-#     for k in i[1:]:
-#         string = string + act_id + "," + k + "\n"
+# file_data_question = []
+# file_data_options = []
+
+# with open("otherpythonstuff/Only_Activity_Database_Entry_file1.csv", "r") as file: # question and option
+#     data = csv.DictReader(file, delimiter=',')
+#     question_id = 0
+#     question_option_id = 0
+#     for i in data:
+#         local_question = [question_id]
+#         keys = list(i.keys())
+#         keys_1 = keys[18:24] # Feedback question 1 [18:24]
+#         keys_2 = keys[24:30] # Feedback question 2 [24:30]
+
+#         response = i[keys_1[1]]
+#         local_question.append(i[keys_1[0]])
+#         local_question.append(response)
+#         if i[keys_1[1]] in ["Checkbox"]:
+#             local_question.append(True)
+#         else:
+#             local_question.append(False)
+#         local_question.append(None)
+#         local_question.append(None)
+#         local_question.append(False)
+#         file_data_question.append(local_question.copy())
+
+#         for k in range(4):
+#             option = i[keys_1[k+2]]
+#             local_option = [question_option_id, question_id]
+#             if option != "None":
+#                 if i[keys_1[1]] in ["Radio Button", "Checkbox"]:
+#                     local_option.append(option)
+#                     local_option.append("Null")
+#                     file_data_options.append(local_option.copy())
+#                     local_option = []
+#                     question_option_id += 1
+
+#         question_id += 1
+
+#         local_question = [question_id]
+#         response1 = i[keys_2[1]]
+#         local_question.append(i[keys_2[0]])
+#         local_question.append(response1)
+#         if i[keys_2[1]] in ["Checkbox"]:
+#             local_question.append(True)
+#         else:
+#             local_question.append(False)
+#         local_question.append(None)
+#         local_question.append(None)
+#         local_question.append(False)
+#         file_data_question.append(local_question.copy())
+        
+#         for k in range(4):
+#             option = i[keys_2[k+2]]
+#             local_option = [question_option_id, question_id]
+#             if option != "None":
+#                 if i[keys_2[1]] in ["Radio Button", "Checkbox"]:
+#                     local_option.append(option)
+#                     local_option.append("Null")
+#                     file_data_options.append(local_option.copy())
+#                     local_option = []
+#                     question_option_id += 1
+    
+#         question_id += 1
+
+# print(file_data_question[0])
+# print(file_data_options[0])
+# print(file_data_options[1])
+
+string = ""
+for i in file_data: # associated and std (one by one)
+    act_id = i[0]
+    # string += str(i[0])
+    for k in i[1:]:
+        string = string + act_id + "," + str(k) + "\n"
 
 # print(string)
-# with open("test.csv", "w") as file1:
-#     file1.write(string)
+
+# string1 = ""
+# for i in file_data_question: # question
+#     # act_id = i[0]
+#     string1 += str(i[0])
+#     for k in i[1:]:
+#         string1 = string1 + "," + str(k)
+#     string1 += "\n"
+
+# string2 = ""
+# for i in file_data_options: # option
+#     # act_id = i[0]
+#     string2 += str(i[0])
+#     for k in i[1:]:
+#         string2 = string2 + "," + str(k)
+#     string2 += "\n"
+
+# print(string1)
+# print(string2)
+# with open("otherpythonstuff/act_to_feedback_question.csv", "w") as file1: # question
+#     file1.write(string1)
+
+# with open("otherpythonstuff/act_to_feedback_option.csv", "w") as file2: # option
+#     file2.write(string2)
+
+# with open("otherpythonstuff/act_to_associated.csv", "w") as file3: # associated
+#     file3.write(string)
+
+# with open("otherpythonstuff/act_to_std.csv", "w") as file4: # std
+#     file4.write(string)
